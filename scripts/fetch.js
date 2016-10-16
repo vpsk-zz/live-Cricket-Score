b92b18529abbbe8b3cc01ceac8be6e8a3b65b4ef
virendra <vpsk05@gmail.com>
2016-10-16 12:45:55 +0530

    removed team tag

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -62,7 +62,6 @@ function display_matchData(data) {
     if (data.matchStarted === true) {
         item = '<div class="post">';
         item += '<span class="tag"></span>\
-                  <div><h4 class="team-detail"><span>Team-1</span> <==> <span>Team-2</span></h4></div>\
                   <div><h4 class="team-name"><span>' + data.team1 + 'Team-1</span> Vs <span>' + data.team1 + '</span></h4></div>\
                   <div class="score">' + data.score + '</div>\
                   <div class="score">' + data.description + '</div>\
